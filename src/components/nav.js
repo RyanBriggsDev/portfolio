8a960b1528e12e7fcf445d1845fd7846b1ab9cde
RyanMatthewBriggs <ryan.briggs248@gmail.com>
2022-06-13 09:29:22 +0100

    cleaned up scss nav page

--- src/components/nav.js
+++ src/components/nav.js
@@ -7,26 +7,28 @@ const NavBar = () => {
     <nav>
         <div className="container">
             <ul>
-                <div className='navHome'>
+                <div className='navLeft'>
                     <a href="index.html" className="navLink">
                         <li className="navListItem">ryanbriggs.dev</li>
                     </a>
                 </div>
-                <div className='navLinks'>
-                    <a href="#projects" className="navLink">
+                <div className='navRight'>
+                    <div className='navLrgDisplayLinks'>
+                    <a href="#projects" className="navLink il-block">
                         <li className="navListItem">Projects</li>
                     </a>
-                    <a href="#about" className="navLink">
+                    <a href="#about" className="navLink il-block">
                         <li className="navListItem">About</li>
                     </a>
-                    <a href="#contact" className="navLink contact btn">
+                    <a href="#contact" className="navLink contact btn il-block">
                         <li className="navListItem">Contact</li>
                     </a>
-                </div>
-                <div className='hamburgerMenu'>
-                    <div className='burgLine'>-</div>
-                    <div className='burgLine'>-</div>
-                    <div className='burgLine'>-</div>
+                    </div>
+                    <div className='hamburgerBtn'>
+                        <div className='burgLine'></div>
+                        <div className='burgLine'></div>
+                        <div className='burgLine'></div>
+                    </div>
                 </div>
             </ul>
         </div>
