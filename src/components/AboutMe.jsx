1dc60012e6fc79bd27b4b102a3b0b4b930d82998
92796110+RyanBriggsDev@users.noreply.github.com <92796110+RyanBriggsDev@users.noreply.github.com>
2022-09-10 20:10:18 +0100

    about me component

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { useState } from 'react'
+
+let data = require('../data/aboutMe.json')
 
 function AboutMe() {
+
+  const [aboutMeText, setAboutMeText] = useState(data.descriptions.fairlyShort)
+
   return (
-    <div>AboutMe</div>
+    <div className='about-me'>
+      <h2>Learn a bit about me, or a lot about me.</h2>
+      <p>{aboutMeText}</p>
+      <div className="desc-option-div">
+        <button className='btn mr-1' onClick={() => setAboutMeText(data.descriptions.short)}>Short</button>
+        <button className='btn mr-1' onClick={() => setAboutMeText(data.descriptions.fairlyShort)}>Fairly Short</button>
+        <button className='btn mr-1' onClick={() => setAboutMeText(data.descriptions.long)}>Long</button>
+        <button className='btn mr-1' onClick={() => setAboutMeText(data.descriptions.longAf)}>Long Af</button>
+      </div>
+    </div>
   )
 }
 
