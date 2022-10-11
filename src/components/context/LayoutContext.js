a54da78766f83aa7b5e0f0e12cce11938c368fe5
92796110+RyanBriggsDev@users.noreply.github.com <92796110+RyanBriggsDev@users.noreply.github.com>
2022-10-11 15:44:47 +0100

    import all repos to repos page

--- src/components/context/LayoutContext.js
+++ src/components/context/LayoutContext.js
@@ -8,8 +8,10 @@ export const LayoutProvider = ({ children }) => {
     const [repos, setRepos] = useState({})
     const [loading, setLoading] = useState(false)
     const [callComplete, setCallComplete] = useState(false)
+    const [allRepos, setAllRepos] = useState({})
+    const [allCallsComplete, setAllCallsComplete] = useState(false)
 
-    // import repos
+    // import 6 repos
     const fetchRepos = async () => {
         setLoading(true)
         const response = await fetch('https://api.github.com/users/ryanbriggsdev/repos?per_page=6&sort=updated')
@@ -19,11 +21,24 @@ export const LayoutProvider = ({ children }) => {
         setCallComplete(true)
     }
 
+    // import all repos
+    const fetchAllRepos = async () => {
+        setLoading(true)
+        const response = await fetch('https://api.github.com/users/ryanbriggsdev/repos?sort=updated')
+        const data = await response.json()
+        setAllRepos(data)
+        setLoading(false)
+        setAllCallsComplete(true)
+    }
+
     return <LayoutContext.Provider value={{
         fetchRepos,
         repos,
         loading,
         callComplete,
+        fetchAllRepos,
+        allRepos,
+        allCallsComplete,
     }}>
         {children}
     </LayoutContext.Provider>
