1a374069a4995ec593b3606d9eeaa3d2538f79b4
92796110+RyanBriggsDev@users.noreply.github.com <92796110+RyanBriggsDev@users.noreply.github.com>
2022-10-22 20:55:20 +0100

    clear cards

--- src/components/layout/ProjectCards.jsx
+++ src/components/layout/ProjectCards.jsx
@@ -18,17 +18,7 @@ function ProjectCards() {
                 <div className="projects-grid">
                     {repos.map((repo) => (
                         <div className="project-card" key={repo.id}>
-                            <div className="project-card-title">
-                                <h3>{repo.name.charAt(0).toUpperCase() + repo.name.slice(1)}.</h3>
-                                <p>{repo.description ? repo.description : <></>}.</p>
-                            </div>
-                            <div className="divider"></div>
-                            {repo.topics[0] && repo.topics[1] ? <p>Made with {repo.topics[0].charAt(0).toUpperCase() + repo.topics[0].slice(1).replaceAll('-', ' ')} and {repo.topics[1].charAt(0).toUpperCase() + repo.topics[1].slice(1).replaceAll('-', ' ')}.</p> : <></>}
-                            
-                            <div className="project-card-links">
-                                <a href={repo.html_url} target='_blank' rel='noreferrer'><button className='link-btn'>View Repo</button></a>
-                                {repo.homepage ? <a href={repo.homepage} target='_blank' rel='noreferrer'><button className='link-btn'>Visit Site</button></a> : <></>}
-                            </div>
+                            {repo.name}
                         </div>
                     ))} 
             </div>
