1e1f3e4f939ecc22f8dac708a2086ceb9c87639c
92796110+RyanBriggsDev@users.noreply.github.com <92796110+RyanBriggsDev@users.noreply.github.com>
2022-10-26 06:57:14 +0100

    version log

--- src/components/pages/Version.jsx
+++ src/components/pages/Version.jsx
@@ -1,4 +1,5 @@
 import Header from '../layout/Header'
+import SectionTitle from '../SectionTitle'
 
 function Version() {
 
@@ -7,15 +8,23 @@ function Version() {
     <div className='content-grid'>
         <Header 
             headerText={`Current Version`} 
-            headerDesc="Version 1.0.0"
+            headerDesc="Version 1.2.0"
             btnText=''
             to=''
             display='none'
         />
         <ul className='version-history'>
             <li>
-                <h2>Version: 1.0.0</h2>
-                <p><strong>Update History: </strong>First version of the portfolio. Updating and adding new features over time as I learn.</p>
+                <SectionTitle text='Version: 1.0.2' />
+                <p><strong>Update Notes: </strong>Added 'skills' component and section on homepage.</p>
+            </li>
+            <li>
+                <SectionTitle text='Version: 1.0.1' />
+                <p><strong>Update Notes: </strong>Re-evaluated home cards section as I wasn't happy with how they looked. Removed GitHub API for this section as it didn't give me the flexibility I wanted.</p>
+            </li>
+            <li>
+                <SectionTitle text='Version: 1.0.0' />
+                <p><strong>Update Notes: </strong>First version of the portfolio. Updating and adding new features over time as I learn.</p>
             </li>
         </ul>
     </div>
