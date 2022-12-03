523022964a51da0c0c8b6954916423a327c0c57e
92796110+RyanBriggsDev@users.noreply.github.com <92796110+RyanBriggsDev@users.noreply.github.com>
2022-12-03 21:50:26 +0000

    about me section buttons

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -13,11 +13,11 @@ function AboutMe() {
   return (
     <div className='about-me'>
       <SectionTitle text='Learn a bit, or a lot, about me' />
-      <div className="desc-option-div">
-        <div className={`btn-circle ${aboutMeText === data.descriptions.short ? 'btn-circle-selected' : ''}`} onClick={() => setAboutMeText(data.descriptions.short)}></div>
-        <div className={`btn-circle ${aboutMeText === data.descriptions.fairlyShort ? 'btn-circle-selected' : ''}`} onClick={() => setAboutMeText(data.descriptions.fairlyShort)}></div>
-        <div className={`btn-circle ${aboutMeText === data.descriptions.long ? 'btn-circle-selected' : ''}`} onClick={() => setAboutMeText(data.descriptions.long)}></div>
-        <div className={`btn-circle ${aboutMeText === data.descriptions.longAf ? 'btn-circle-selected' : ''}`} onClick={() => setAboutMeText(data.descriptions.longAf)}></div>
+      <div className="flex f-start f-row">
+        <div className={`btn-circle p-1 ${aboutMeText === data.descriptions.short ? 'btn-circle-selected' : ''}`} onClick={() => setAboutMeText(data.descriptions.short)}></div>
+        <div className={`btn-circle p-1 ${aboutMeText === data.descriptions.fairlyShort ? 'btn-circle-selected' : ''}`} onClick={() => setAboutMeText(data.descriptions.fairlyShort)}></div>
+        <div className={`btn-circle p-1 ${aboutMeText === data.descriptions.long ? 'btn-circle-selected' : ''}`} onClick={() => setAboutMeText(data.descriptions.long)}></div>
+        <div className={`btn-circle p-1 ${aboutMeText === data.descriptions.longAf ? 'btn-circle-selected' : ''}`} onClick={() => setAboutMeText(data.descriptions.longAf)}></div>
       </div>
       <p>{aboutMeText}</p>
     </div>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import App from './App';
 import './styles/index.scss'
 import './styles/cards.scss'
 import './styles/skills.scss'
+import './styles/text.scss'
+import './styles/buttons.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
