56dc7a7838e63e8a1083a471811c8f60ade99ef7
92796110+RyanBriggsDev@users.noreply.github.com <92796110+RyanBriggsDev@users.noreply.github.com>
2022-12-04 17:22:30 +0000

    updated version page

--- src/pages/Version.jsx
+++ src/pages/Version.jsx
@@ -8,7 +8,7 @@ function Version() {
     <div className='content-grid'>
         <Header 
             headerText={`Current Version`} 
-            headerDesc="Version 1.2.0"
+            headerDesc="1.0.3"
             btnText=''
             to=''
             display='none'
@@ -16,20 +16,20 @@ function Version() {
         <ul className='version-history'>
             <li>
                 <SectionTitle text='Version: 1.0.3' />
-                <p><strong>Update Notes: </strong>Added 'skills' component and section on homepage.</p>
+                <p className='font-1_2'><strong>Update Notes: </strong>Carried out a refactor of code file structure to make app easier to work on.</p>
             </li>
             <li>
-                <SectionTitle text='Version: 1.0.3' />
-                <p><strong>Update Notes: </strong>Carried out a refactor of code file structure to make app easier to work on.</p>
+                <SectionTitle text='Version: 1.0.2' />
+                <p className='font-1_2'><strong>Update Notes: </strong>Added 'skills' component and section on homepage.</p>
             </li>
 
             <li>
                 <SectionTitle text='Version: 1.0.1' />
-                <p><strong>Update Notes: </strong>Re-evaluated home cards section as I wasn't happy with how they looked. Removed GitHub API for this section as it didn't give me the flexibility I wanted.</p>
+                <p className='font-1_2'><strong>Update Notes: </strong>Re-evaluated home cards section as I wasn't happy with how they looked. Removed GitHub API for this section as it didn't give me the flexibility I wanted.</p>
             </li>
             <li>
                 <SectionTitle text='Version: 1.0.0' />
-                <p><strong>Update Notes: </strong>First version of the portfolio. Updating and adding new features over time as I learn.</p>
+                <p className='font-1_2'><strong>Update Notes: </strong>First version of the portfolio. Updating and adding new features over time as I learn.</p>
             </li>
         </ul>
     </div>
